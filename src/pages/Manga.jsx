89b70540a92de8c16af975557d6ffca162f7c256
chia <chidinmaehirim002@gmail.com>
2025-04-01 02:04:22 -0600

    added file

--- src/pages/Manga.jsx
+++ src/pages/Manga.jsx
@@ -1,10 +1,20 @@
 import cover from '../assets/img/manga.svg'
+import { products } from '../products'
+import Cart from '../components/Cart'
 
 const Manga = () => {
   return (
     <div>
       <img src={cover} alt="" className='manga-cover'/>
+    <div className='manga-container'>
+      <div className='manga-item'>
+        {products.map((product, key) => 
+        <Cart key={key} data={product}/>
+      )}
+      </div>
     </div>
+    </div>
+
     
   )
 }
---
+++ src/products.js
@@ -0,0 +1,43 @@
+import manga1 from "./assets/img/manga-1.png"
+import manga2 from "./assets/img/manga-2.jpg"
+import manga3 from "./assets/img/manga-3.jpg"
+import manga4 from "./assets/img/manga-4.jpg"
+
+export const products = [
+  {
+    id: 1,
+    name: 'Firefly Wedding',
+    author: 'Oreco Tachibana',
+    price: 101,
+    image: manga1,
+    description: 'Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor ipsum culpa Lorem ipsum ullamco ullamco dolor sit do. Do proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+    slug: 'firefly-wedding'
+  },
+  {
+    id: 2,
+    name: 'Skip and Loafer',
+    author: 'Misaki Takamatsu',
+    price: 99,
+    image: manga2,
+    description: 'Skip and Loafer follows a smart but naive girl as she moves from the countryside to Tokyo, navigating school life with charm, awkwardness, and new friendships.',
+    slug: 'skip-and-loafer'
+  },
+  {
+    id: 3,
+    name: 'Ashi-Girl',
+    author: 'Kozueko Morimoto',
+    price: 89,
+    image: manga3,
+    description: 'Ashi-Girl is a historical romance where a modern-day girl travels back in time to the Warring States period, discovering love and danger in an unfamiliar world.',
+    slug: 'ashi-girl'
+  },
+  {
+    id: 4,
+    name: 'My Love Mix-Up',
+    author: 'Wataru Hinekure',
+    price: 94,
+    image: manga4,
+    description: 'A fun and heartfelt story of misunderstandings, high school crushes, and sweet LGBTQ+ romance that begins with a love triangle and leads to self-discovery.',
+    slug: 'my-love-mix-up'
+  }
+]
